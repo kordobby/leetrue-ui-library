55e1c5278c3bfe001d056a6d844c8506f002d165
이트루 <leetrue@iteuluui-MacBookPro.local>
2023-11-27 23:13:17 +0900

    install. emotion

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    tsconfigPaths(),
+  ],
 });
