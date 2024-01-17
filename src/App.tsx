fe919a780f1b277dfe9f785d2a9a0b91e1ab69de
kordobby <poordobby@naver.com>
2024-01-17 18:22:05 +0900

    test. test for auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Sider } from "components/layout/Sider";
 import { Main } from "components/layout/Main";
 import { Body } from "components/layout/Body";
 // eslint-disable-next-line import/no-extraneous-dependencies
+// auth test
 import { BrowserRouter } from "react-router-dom";
 import Router from "libs/router/Router";
 
